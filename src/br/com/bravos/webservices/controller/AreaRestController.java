44562dde9a5ed634b31fe0a47f5bee18367ace91
jamesson <jamesson.sena2009@hotmail.com>
2016-11-07 23:16:38 -0200

    Adição da Annotation CrossOrigin para o funcionamento da intergração com o front-end [react js]. Adição de mais um tipo de erro de retorno da propriedade. Adição da exceção de token para cadastrarUsuario. Adição do retorno HttpStatus OK para as consultas do usuarios

--- src/br/com/bravos/webservices/controller/AreaRestController.java
+++ src/br/com/bravos/webservices/controller/AreaRestController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,6 +19,7 @@ import br.com.bravos.webservices.model.AreaBean;
  * @author WeltonBatista
  *
  */
+@CrossOrigin
 @RestController
 public class AreaRestController implements _TratamentoRetorno {
 	
